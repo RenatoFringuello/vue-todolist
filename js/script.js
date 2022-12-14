a4e43dc4451ba28cc93b5a2d6827649be0202ae5
RenatoFringuello <renatofringuello@hotmail.it>
2022-12-14 16:49:42 +0100

    set up repo

---
+++ js/script.js
@@ -0,0 +1,38 @@
+const {createApp} = Vue;
+
+createApp({
+    data(){
+        return{
+            thingToAdd : '.', 
+            toDoList : [
+                {text:'dnkejrvndjlsnevr', done:true},
+                {text:'dnkejrvndjlsnevr', done:true},
+                {text:'dnkejrvndjlsnevr', done:false},
+                {text:'dnkejrvndjlsnevr', done:false},
+                {text:'dnkejrvndjlsnevr', done:true},
+                {text:'dnkejrvndjlsnevr', done:true},
+                {text:'dnkejrvndjlsnevr', done:true},
+                {text:'dnkejrvndjlsnevr', done:false},
+                {text:'dnkejrvndjlsnevr', done:true},
+                {text:'dnkejrvndjlsnevr', done:false},
+                {text:'dnkejrvndjlsnevr', done:true},
+                {text:'dnkejrvndjlsnevr', done:true},
+                {text:'dnkejrvndjlsnevr', done:true},
+            ]
+        }
+    },
+    methods : {
+        addItemInList(){
+            if(this.thingToAdd !== '' && this.thingToAdd.length >= 2){
+                this.toDoList.push({text: this.thingToAdd, done:false});
+                this.thingToAdd = '';
+            }
+        },
+        removeItemInList(index){
+            this.toDoList.splice(index, 1);
+        },
+        toggleStrikeOff(index){
+            this.toDoList[index].done = !this.toDoList[index].done;
+        }
+    }
+}).mount('#app');
